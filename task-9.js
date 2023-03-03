71da34671f1a00cc71ecfa2804d631872933c1d9
kishore6382 <kishorekumar06047@gmail.com>
2023-03-02 22:52:07 -0800

    update

---
+++ task-9.js
@@ -0,0 +1,48 @@
+
+
+		 const users = [
+					  {
+						first_name: 'Mike',
+						last_name: 'Sheridan'
+					  },
+					  {
+						first_name: 'Tim',
+						last_name: 'Lee'
+					  },
+					  {
+						first_name: 'John',
+						last_name: 'Carte'
+					  }
+					];
+			let name=users.map (function (kishore){
+			 return kishore.first_name+kishore.last_name;
+			}
+			)
+			console.log(name);
+			document.write(name);
+			
+			
+			
+			
+		
+			
+			
+			
+			
+			
+		
+			
+			
+			
+			
+			
+		
+			
+			
+			
+			
+		
+			
+			
+			
+	
